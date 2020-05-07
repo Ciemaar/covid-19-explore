46ff8bc21ec770b934a02abaef779a7af473ad1c
Andy Fundinger <andriod@gmail.com>
2020-05-06 22:37:03 -0400

    refactored functions out of notebook for reuse

---
+++ analysis.py
@@ -0,0 +1,25 @@
+from datetime import timedelta
+
+import pandas as pd
+
+from data_access import last_full_date
+
+
+def calculate_discrete_derivatives(inframe,column):
+    df = inframe.pivot(index='date', columns='combined_name', values=column)
+    deltas = pd.DataFrame([df[c].diff() for c in df.columns]).transpose()
+    deltas.fillna(0, inplace=True)
+    smoothed_deltas = deltas.rolling(5).mean()
+
+    ddeltas = pd.DataFrame([smoothed_deltas[c].diff() for c in smoothed_deltas.columns]).transpose()
+    ddeltas.fillna(0, inplace=True)
+    smoothed_ddeltas = ddeltas.rolling(5).mean()
+
+    dddeltas = pd.DataFrame([smoothed_ddeltas[c].diff() for c in smoothed_ddeltas.columns]).transpose()
+    dddeltas.fillna(0, inplace=True)
+    smoothed_dddeltas = dddeltas.rolling(5).mean()
+
+    predicted_deltas = pd.DataFrame({last_full_date + timedelta(days=day): smoothed_deltas.loc[last_full_date] +
+                                                                           smoothed_ddeltas.loc[last_full_date] * day for day in range(-7, 7)}).transpose()
+    predicted_deltas.index.name = 'date'
+    return deltas, smoothed_deltas, ddeltas, smoothed_ddeltas, dddeltas, smoothed_dddeltas, predicted_deltas
---
+++ data_access.py
@@ -0,0 +1,70 @@
+from datetime import timedelta
+
+import pandas
+import pandas as pd
+import pymongo
+from pymongo import MongoClient
+
+MDB_URL = "mongodb+srv://readonly:readonly@covid-19.hip2i.mongodb.net/covid19"
+client = MongoClient(MDB_URL)
+db = client.get_database("covid19")
+stats = db.get_collection("global_and_us")
+metadata = db.get_collection("metadata")
+meta = metadata.find_one()
+last_date = meta["last_date"]
+
+
+def snag_data(columns=("date", "confirmed", "deaths",'state'),**filters):
+    df = pd.DataFrame(
+        stats.find(filters)
+        .sort("state", pymongo.DESCENDING)
+    )
+    if 'uid' in df.columns:
+        del df['uid']
+    if 'fips' in df.columns:
+        del df['fips']
+    if 'country_code' in df.columns:
+        del df['country_code']
+    df['deaths'] = df['deaths'].astype('Int64')
+    df['confirmed'] = df['confirmed'].astype('Int64')
+    df['population'] = df['population'].astype('Int64')
+    return df
+
+
+def get_for_country_day(country='US',stat_date=None,**filters):
+    if stat_date is None:
+        stat_date = last_date
+
+    filters.update({'country': country, "date": stat_date})
+    return snag_data(**filters)
+
+
+EARTH_RADIUS = 6371.0 # This is used in the $geoWithin query later.
+
+
+def near_by_data(query_date=None,longitude=-74.114202,latitude=40.6737968,distance_km=250.0,group_by='state'):
+    if query_date is None:
+        query_date = last_full_date
+    results = stats.find(
+        {
+            "date": query_date,
+            "loc": {
+                "$geoWithin": {
+                    "$centerSphere": [[longitude, latitude], distance_km / EARTH_RADIUS]
+                }
+            },
+        }
+    )
+    df = pd.DataFrame(results)
+    if df.empty:
+        return df
+    df = df.groupby(group_by).sum()
+    del df['uid']
+    del df['fips']
+    del df['country_code']
+    df['per_capita_deaths']    = df['deaths']/df['population']
+    df['per_capita_confirmed'] = df['confirmed']/df['population']
+    return df
+
+
+last_full_date = last_date - timedelta(days=1)
